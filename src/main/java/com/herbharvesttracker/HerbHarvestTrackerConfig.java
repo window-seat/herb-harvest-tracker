170d3c0028207d6e477085b31ec3207f62e62d00
Aaron <aaron.m.macariola@gmail.com>
2025-03-30 21:17:44 -0700

    More refactors.

--- src/main/java/com/herbharvesttracker/HerbHarvestTrackerConfig.java
+++ src/main/java/com/herbharvesttracker/HerbHarvestTrackerConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("herb")
+@ConfigGroup("herbharvesttracker")
 public interface HerbHarvestTrackerConfig extends Config
 {
 	@ConfigItem(
